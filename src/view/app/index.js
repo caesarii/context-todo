c22b329e93feb26aba2534f92a20ed1329c73852
qinghe <iwangqinghe@gmail.com>
2019-10-30 09:33:06 +0800

    将状态 todos 改造为 todosContext

--- src/view/app/index.js
+++ src/view/app/index.js
@@ -7,95 +7,92 @@ import Main from '../main'
 import Footer from '../footer'
 import './index.css'
 
+ 
+const onAdd = (text, todos) => {
+    if (text === '') {
+        return
+    }
+    const id = Counter.increment()
+    const todo = new Todo({
+        id,
+        text: text,
+        complete: false,
+    })
+    
+    todos.push(todo)
+    return todos
+}
+
+const onDeleteTodo = (id, todos) => {
+    todos = todos.filter(t => {
+        if(t.id !== id) {
+            return t
+        }
+    })
+    return {todos}
+}
+
+const onEditTodo = (id, text, todos) => {
+    const todos = prev.todos
+    todos.forEach(t => {
+        if(t.id === id) {
+            t.text = text
+        }
+    })
+    return {todos}
+}
+
+const onToggleTodo = (id, todos) => {
+    todos = todos.map(t => {
+        if(t.id === id) {
+            const completed = t.complete
+            t.complete = !completed
+        }
+        return t
+    })
+    return {todos}
+}
+
+const onToggleAllTodos = (todos) => {
+    const todos = prev.todos.map(t => {
+        t.complete = !t.complete
+        return t
+    })
+    return {todos}
+}
+
+const onDeleteCompletedTodos = (todos) => {
+    const todos = prev.todos.filter(t => {
+        if(!t.complete) {
+            return t
+        }
+    })
+    return {todos}
+}
+const TodosContext = React.createContext(
+    {
+        toods: [],
+        onAdd,
+        onDeleteTodo,
+        onEditTodo,
+        onToggleTodo,
+        onToggleAllTodos,
+        onDeleteCompletedTodos,
+    }
+)
+const editingContext = React.createContext(false)
+const draftContext = React.createContext(null)
+
 class AppView extends Component {
     constructor (props) {
         super(props)
-        const todos = []
         this.state = {
-            todos: todos,
             editing: false,
             draft: null,
         }
         
     }
-    
-    onAdd = (text) => {
-        if (text === '') {
-            return
-        }
-        const id = Counter.increment()
-        this.setState(prev => {
-            const todo = new Todo({
-                id,
-                text: text,
-                complete: false,
-            })
-            
-            prev.todos.push(todo)
-            return prev.todos
-        })
-    }
-    
-    onDeleteTodo = (id) => {
-        this.setState(prev => {
-            let todos = prev.todos
-            todos = todos.filter(t => {
-                if(t.id !== id) {
-                    return t
-                }
-            })
-            return {todos}
-        })
-    }
-    
-    onEditTodo = (id, text) => {
-        this.setState(prev => {
-            const todos = prev.todos
-            todos.forEach(t => {
-                if(t.id === id) {
-                    t.text = text
-                }
-            })
-            return {todos}
-        })
-    }
-    
-    onToggleTodo = (id) => {
-        this.setState(prev => {
-            let todos = prev.todos
-            todos = todos.map(t => {
-                if(t.id === id) {
-                    const completed = t.complete
-                    t.complete = !completed
-                }
-                return t
-            })
-            
-            return {todos}
-        })
-    }
-    
-    onToggleAllTodos = () => {
-        this.setState(prev => {
-            const todos = prev.todos.map(t => {
-                t.complete = !t.complete
-                return t
-            })
-            return {todos}
-        })
-    }
-    
-    onDeleteCompletedTodos = () => {
-        this.setState(prev => {
-            const todos = prev.todos.filter(t => {
-                if(!t.complete) {
-                    return t
-                }
-            })
-            return {todos}
-        })
-    }
-    
+   
     render () {
         const { todos } = this.state
         const { onAdd, onUpdateDraft, onDeleteTodo, onEditTodo, onToggleTodo, onToggleAllTodos, onDeleteCompletedTodos} = this
