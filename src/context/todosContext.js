f819b294f0d878c840f776c75d4cb43c5c588d9e
qinghe <iwangqinghe@gmail.com>
2019-10-30 10:23:07 +0800

    用错了, 重构

--- src/context/todosContext.js
+++ src/context/todosContext.js
@@ -1,77 +1,15 @@
 
 import React from 'react'
-import Counter from '../utils/Counter'
-import Todo from '../utils/Todo'
 
-const onAdd = (text, todos) => {
-    if (text === '') {
-        return
-    }
-    const id = Counter.increment()
-    const todo = new Todo({
-        id,
-        text: text,
-        complete: false,
-    })
-    
-    todos.push(todo)
-    return todos
+const todosContext = {
+    todos: [],
+    onAdd: () => {},
+    onDeleteTodo: () => {},
+    onEditTodo: () => {},
+    onToggleTodo: () => {},
+    onToggleAllTodos: () => {},
+    onDeleteCompletedTodos: () => {}
 }
-
-const onDeleteTodo = (id, todos) => {
-    todos = todos.filter(t => {
-        if(t.id !== id) {
-            return t
-        }
-    })
-    return {todos}
-}
-
-const onEditTodo = (id, text, todos) => {
-    todos.forEach(t => {
-        if(t.id === id) {
-            t.text = text
-        }
-    })
-    return {todos}
-}
-
-const onToggleTodo = (id, todos) => {
-    todos = todos.map(t => {
-        if(t.id === id) {
-            const completed = t.complete
-            t.complete = !completed
-        }
-        return t
-    })
-    return {todos}
-}
-
-const onToggleAllTodos = (todos) => {
-    todos = todos.map(t => {
-        t.complete = !t.complete
-        return t
-    })
-    return {todos}
-}
-
-const onDeleteCompletedTodos = (todos) => {
-    todos = todos.filter(t => {
-        if(!t.complete) {
-            return t
-        }
-    })
-    return {todos}
-}
-export const todos = [];
 export const TodosContext = React.createContext(
-    {
-        todos,
-        onAdd,
-        onDeleteTodo,
-        onEditTodo,
-        onToggleTodo,
-        onToggleAllTodos,
-        onDeleteCompletedTodos,
-    }
+    todosContext
 )
--- src/view/app/index.js
+++ src/view/app/index.js
@@ -1,26 +1,122 @@
 import React, { Component } from 'react'
 import Immutable from 'immutable'
-
-
+import Counter from '../../utils/Counter'
+import Todo from '../../utils/Todo'
 import Header from '../header'
 import Main from '../main'
 import Footer from '../footer'
-import { todos, TodosContext } from '../../context/todosContext'
+import { TodosContext } from '../../context/todosContext'
 import './index.css'
 
 class AppView extends Component {
     constructor (props) {
         super(props)
+        const {
+            onAdd,
+            onDeleteTodo,
+            onEditTodo,
+            onToggleTodo,
+            onToggleAllTodos,
+            onDeleteCompletedTodos
+        } = this;
+        this.state = {
+            todos: [],
+            onAdd,
+            onDeleteTodo,
+            onEditTodo,
+            onToggleTodo,
+            onToggleAllTodos,
+            onDeleteCompletedTodos
+        }
+
     }
+
+    onAdd = (text) => {
+        if (text === '') {
+            return
+        }
+        const id = Counter.increment()
+        this.setState(prev => {
+            const todo = new Todo({
+                id,
+                text: text,
+                complete: false,
+            })
+            
+            prev.todos.push(todo)
+            return prev.todos
+        })
+    }
+    
+    onDeleteTodo = (id) => {
+        this.setState(prev => {
+            let todos = prev.todos
+            todos = todos.filter(t => {
+                if(t.id !== id) {
+                    return t
+                }
+            })
+            return {todos}
+        })
+    }
+    
+    onEditTodo = (id, text) => {
+        this.setState(prev => {
+            const todos = prev.todos
+            todos.forEach(t => {
+                if(t.id === id) {
+                    t.text = text
+                }
+            })
+            return {todos}
+        })
+    }
+    
+    onToggleTodo = (id) => {
+        this.setState(prev => {
+            let todos = prev.todos
+            todos = todos.map(t => {
+                if(t.id === id) {
+                    const completed = t.complete
+                    t.complete = !completed
+                }
+                return t
+            })
+            
+            return {todos}
+        })
+    }
+    
+    onToggleAllTodos = () => {
+        this.setState(prev => {
+            const todos = prev.todos.map(t => {
+                t.complete = !t.complete
+                return t
+            })
+            return {todos}
+        })
+    }
+    
+    onDeleteCompletedTodos = () => {
+        this.setState(prev => {
+            const todos = prev.todos.filter(t => {
+                if(!t.complete) {
+                    return t
+                }
+            })
+            return {todos}
+        })
+    }
+
    
     render () {
         return (
           <div className="container">
-              <TodosContext.Provider value={todos}>
+              <TodosContext.Provider value={this.state}>
                 <div className="todoapp">
-                    {/* <Header {...props} /> */}
+                    <Header />
                     <Main />
-                    {/* <Footer {...props} /> */}
+                    <Footer />
                 </div>
               </TodosContext.Provider>
               <div className="info">
