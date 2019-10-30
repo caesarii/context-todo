b4e76e86b81267d1e910f5cdd1adc15c0b3714a3
qinghe <iwangqinghe@gmail.com>
2019-10-30 09:42:34 +0800

    将 todosContext 单独的文件

---
+++ src/context/todosContext.js
@@ -0,0 +1,76 @@
+
+import React from 'react'
+
+const onAdd = (text, todos) => {
+    if (text === '') {
+        return
+    }
+    const id = Counter.increment()
+    const todo = new Todo({
+        id,
+        text: text,
+        complete: false,
+    })
+    
+    todos.push(todo)
+    return todos
+}
+
+const onDeleteTodo = (id, todos) => {
+    todos = todos.filter(t => {
+        if(t.id !== id) {
+            return t
+        }
+    })
+    return {todos}
+}
+
+const onEditTodo = (id, text, todos) => {
+    const todos = prev.todos
+    todos.forEach(t => {
+        if(t.id === id) {
+            t.text = text
+        }
+    })
+    return {todos}
+}
+
+const onToggleTodo = (id, todos) => {
+    todos = todos.map(t => {
+        if(t.id === id) {
+            const completed = t.complete
+            t.complete = !completed
+        }
+        return t
+    })
+    return {todos}
+}
+
+const onToggleAllTodos = (todos) => {
+    const todos = prev.todos.map(t => {
+        t.complete = !t.complete
+        return t
+    })
+    return {todos}
+}
+
+const onDeleteCompletedTodos = (todos) => {
+    const todos = prev.todos.filter(t => {
+        if(!t.complete) {
+            return t
+        }
+    })
+    return {todos}
+}
+export const todos = [];
+export const TodosContext = React.createContext(
+    {
+        todos,
+        onAdd,
+        onDeleteTodo,
+        onEditTodo,
+        onToggleTodo,
+        onToggleAllTodos,
+        onDeleteCompletedTodos,
+    }
+)
--- src/view/app/index.js
+++ src/view/app/index.js
@@ -5,81 +5,10 @@ import Todo from '../../utils/Todo'
 import Header from '../header'
 import Main from '../main'
 import Footer from '../footer'
+import { todos, todosContext } from '../../context/todosContext'
 import './index.css'
 
  
-const onAdd = (text, todos) => {
-    if (text === '') {
-        return
-    }
-    const id = Counter.increment()
-    const todo = new Todo({
-        id,
-        text: text,
-        complete: false,
-    })
-    
-    todos.push(todo)
-    return todos
-}
-
-const onDeleteTodo = (id, todos) => {
-    todos = todos.filter(t => {
-        if(t.id !== id) {
-            return t
-        }
-    })
-    return {todos}
-}
-
-const onEditTodo = (id, text, todos) => {
-    const todos = prev.todos
-    todos.forEach(t => {
-        if(t.id === id) {
-            t.text = text
-        }
-    })
-    return {todos}
-}
-
-const onToggleTodo = (id, todos) => {
-    todos = todos.map(t => {
-        if(t.id === id) {
-            const completed = t.complete
-            t.complete = !completed
-        }
-        return t
-    })
-    return {todos}
-}
-
-const onToggleAllTodos = (todos) => {
-    const todos = prev.todos.map(t => {
-        t.complete = !t.complete
-        return t
-    })
-    return {todos}
-}
-
-const onDeleteCompletedTodos = (todos) => {
-    const todos = prev.todos.filter(t => {
-        if(!t.complete) {
-            return t
-        }
-    })
-    return {todos}
-}
-const TodosContext = React.createContext(
-    {
-        toods: [],
-        onAdd,
-        onDeleteTodo,
-        onEditTodo,
-        onToggleTodo,
-        onToggleAllTodos,
-        onDeleteCompletedTodos,
-    }
-)
 const editingContext = React.createContext(false)
 const draftContext = React.createContext(null)
 
@@ -94,27 +23,15 @@ class AppView extends Component {
     }
    
     render () {
-        const { todos } = this.state
-        const { onAdd, onUpdateDraft, onDeleteTodo, onEditTodo, onToggleTodo, onToggleAllTodos, onDeleteCompletedTodos} = this
-        const props = {
-            todos,
-            onAdd,
-            onUpdateDraft,
-            onToggleAllTodos,
-            onDeleteTodo,
-            onEditTodo,
-            onToggleTodo,
-            onDeleteCompletedTodos,
-        }
-        
         return (
           <div className="container">
-              <div className="todoapp">
-                  <Header {...props} />
-                  <Main {...props} />
-                  <Footer {...props} />
-              
-              </div>
+              <TodosContext.Provider value={todos}>
+                <div className="todoapp">
+                    <Header {...props} />
+                    <Main {...props} />
+                    <Footer {...props} />
+                </div>
+              </TodosContext.Provider>
               <div className="info">
                   <p>Double-click to edit a todo</p>
                   <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
