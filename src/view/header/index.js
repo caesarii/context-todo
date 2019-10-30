38264d850f04df80f13cd3c4cf9dbd8d63063c55
qinghe <iwangqinghe@gmail.com>
2019-10-30 10:23:41 +0800

    在 header 中消费context

--- src/view/header/index.js
+++ src/view/header/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './index.css'
+import { TodosContext } from '../../context/todosContext';
 
-function Header (props) {
+function Header () {
     return (
       <header className='header'>
           <h1 className='title'>Todos</h1>
-          <NewTodo {...props}/>
+          <NewTodo/>
       </header>
     )
 }
@@ -25,36 +26,46 @@ class NewTodo extends React.Component {
     onOver = () => {
         this.setState({ draft: '' })
     }
-    // 停止编辑
-    onBlur = () => {
-        this.props.onAdd(this.state.draft)
-        this.onOver()
-    }
     
-    // 编辑完成
-    onKeyDown = (e) => {
-        const ENTER_KEY_CODE = 13
-        if (e.keyCode === ENTER_KEY_CODE) {
-            this.props.onAdd(this.state.draft)
-            this.onOver()
-            
-        }
-    }
     
     render () {
         const { state, onBlur, onChange, onKeyDown } = this
         const { draft } = state
         return (
-          <input
-            className="new-todo"
-            type="text"
-            autoFocus='autoFocus'
-            placeholder="What needs to be done?"
-            value={draft}
-            onBlur={onBlur}
-            onChange={onChange}
-            onKeyDown={onKeyDown}
-          />
+            <TodosContext.Consumer>
+                {
+                    ({ todos, onAdd }) => {
+
+                        // 停止编辑
+                        const onBlur = () => {
+                            onAdd(this.state.draft, todos)
+                            this.onOver()
+                        }
+                        
+                        // 编辑完成
+                        const onKeyDown = (e) => {
+                            const ENTER_KEY_CODE = 13
+                            if (e.keyCode === ENTER_KEY_CODE) {
+                                onAdd(this.state.draft, todos)
+                                this.onOver()
+                                
+                            }
+                        }
+                        return (
+                            <input
+                                className="new-todo"
+                                type="text"
+                                autoFocus='autoFocus'
+                                placeholder="What needs to be done?"
+                                value={draft}
+                                onBlur={onBlur}
+                                onChange={onChange}
+                                onKeyDown={onKeyDown}
+                            />
+                        )
+                    }
+                }
+            </TodosContext.Consumer>
         )
     }
 }
