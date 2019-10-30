c26a4b99dfd9798d23ac62d9cd945f107ec42620
qinghe <iwangqinghe@gmail.com>
2019-10-30 10:23:24 +0800

    在 footer 中消费context

--- src/view/footer/index.js
+++ src/view/footer/index.js
@@ -1,37 +1,46 @@
 import React from 'react'
+import { TodosContext } from '../../context/todosContext';
 import './index.css'
 
-function Footer(props) {
-    const {todos, onDeleteCompletedTodos} = props
-    if(todos.size === 0) {
-        return null
-    }
-    const remaining = todos.filter((todo) => {
-        return !todo.complete
-    }).length
-    const completed = todos.length - remaining
-    const phrase = remaining === 1 ? ' item left' : ' items left'
-
-    let clearCompletedButton = null
-    if(completed > 0) {
-        clearCompletedButton =
-            <button
-                className='clear-completed'
-                onClick={onDeleteCompletedTodos}
-            >
-               Clear completed ({completed})
-            </button>
-    }
+function Footer() {
     return (
-        <footer className="footer">
-            <span className="todo-count">
-                <strong>
-                    {remaining}
-                </strong>
-                {phrase}
-            </span>
-            {clearCompletedButton}
-        </footer>
+        <TodosContext.Consumer>
+            {
+                ({ todos, onDeleteCompletedTodos }) => {
+                    if(todos.size === 0) {
+                        return null
+                    }
+                    const remaining = todos.filter((todo) => {
+                        return !todo.complete
+                    }).length
+                    const completed = todos.length - remaining
+                    const phrase = remaining === 1 ? ' item left' : ' items left'
+                
+                    let clearCompletedButton = null
+                    if(completed > 0) {
+                        clearCompletedButton =
+                            <button
+                                className='clear-completed'
+                                onClick={onDeleteCompletedTodos}
+                            >
+                               Clear completed ({completed})
+                            </button>
+                    }
+                    return (
+                        <footer className="footer">
+                            <span className="todo-count">
+                                <strong>
+                                    {remaining}
+                                </strong>
+                                {phrase}
+                            </span>
+                            {clearCompletedButton}
+                        </footer>
+                    )
+                }
+            }
+            
+        </TodosContext.Consumer>
     )
 }
 
