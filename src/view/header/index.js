9d8506f41f4ead69e7a0c247d1ed7556e0816fbd
jiangzj1008 <jiangzj1008@outlook.com>
2019-10-31 11:17:21 +0800

    使用useContext

--- src/view/header/index.js
+++ src/view/header/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react'
 import './index.css'
 import { TodosContext } from '../../context/todosContext';
 
@@ -16,42 +16,34 @@ function NewTodo(props) {
         draft,
         setDraft
     ] = useState('')
+    const { todos, onAdd } = useContext(TodosContext)
+
+    // 停止编辑
+    const onBlur = () => {
+        onAdd(draft, todos)
+        setDraft('')
+    }
+
+    // 编辑完成
+    const onKeyDown = (e) => {
+        const ENTER_KEY_CODE = 13
+        if (e.keyCode === ENTER_KEY_CODE) {
+            onAdd(draft, todos)
+            setDraft('')
+        }
+    }
 
     return (
-        <TodosContext.Consumer>
-            {
-                ({ todos, onAdd }) => {
-
-                    // 停止编辑
-                    const onBlur = () => {
-                        onAdd(draft, todos)
-                        setDraft('')
-                    }
-
-                    // 编辑完成
-                    const onKeyDown = (e) => {
-                        const ENTER_KEY_CODE = 13
-                        if (e.keyCode === ENTER_KEY_CODE) {
-                            onAdd(draft, todos)
-                            setDraft('')
-                        }
-                    }
-
-                    return (
-                        <input
-                            className="new-todo"
-                            type="text"
-                            autoFocus='autoFocus'
-                            placeholder="What needs to be done?"
-                            value={draft}
-                            onBlur={onBlur}
-                            onChange={(e) => setDraft(e.target.value)}
-                            onKeyDown={onKeyDown}
-                        />
-                    )
-                }
-            }
-        </TodosContext.Consumer>
+        <input
+            className="new-todo"
+            type="text"
+            autoFocus='autoFocus'
+            placeholder="What needs to be done?"
+            value={draft}
+            onBlur={onBlur}
+            onChange={(e) => setDraft(e.target.value)}
+            onKeyDown={onKeyDown}
+        />
     )
 }
 
